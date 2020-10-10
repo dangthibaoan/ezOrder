0e1a080b0504a8d86f2bb281298871a9829cca29
Bảo An <66411880+dangthibaoan@users.noreply.github.com>
2020-10-10 09:39:01 +0700

    update

--- app/src/main/java/com/example/ezorder/ScreenAdminFood.java
+++ app/src/main/java/com/example/ezorder/ScreenAdminFood.java
@@ -2,13 +2,44 @@ package com.example.ezorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScreenAdminFood extends AppCompatActivity {
-
+    private ListView lv;
+    private List<String> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_admin_food);
+
+        lv= (ListView) findViewById(R.id.list_item);
+        list = new ArrayList<>();
+        list.add("Cơm");
+        list.add("Cháo");
+        list.add("Phở");
+        list.add("Mỳ");
+
+        ArrayAdapter adt = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list);
+
+        lv.setAdapter(adt);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Demo
+                startActivity(new Intent(ScreenAdminFood.this,ScreenAdminFoodInfo.class));
+            }
+        });
+    }
+    public void onClick(android.view.View View){
+        //Demo
+        startActivity(new Intent(ScreenAdminFood.this,ScreenAdminFoodInfo.class));
     }
 }
--- app/src/main/java/com/example/ezorder/ScreenAdminTable.java
+++ app/src/main/java/com/example/ezorder/ScreenAdminTable.java
@@ -5,26 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScreenAdminTable extends AppCompatActivity {
-
+    private ListView lv;
+    private List<String> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_admin_table);
 
-        Button btnAddTable = findViewById(R.id.btnAddTable);
-        btnAddTable.setOnClickListener(new View.OnClickListener() {
+        lv= (ListView) findViewById(R.id.list_item);
+        list = new ArrayList<>();
+        list.add("Cơm");
+        list.add("Cháo");
+        list.add("Phở");
+        list.add("Mỳ");
+
+        ArrayAdapter adt = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list);
+
+        lv.setAdapter(adt);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ScreenAdminTable.this, ScreenAdminTableInfo.class));
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Demo
+                startActivity(new Intent(ScreenAdminTable.this,ScreenAdminTableInfo.class));
             }
         });
+    }
 
-        //sự kiện hiển thị list bàn ăn từ database sqlite
-
-        //sự kiện click vào 1 item trong list bàn ăn
-
+    public void onClick(android.view.View View){
+        //Demo
+        startActivity(new Intent(ScreenAdminTable.this,ScreenAdminTableInfo.class));
     }
 }
